00a4f7eca01917ca859e0596f53feb884b69bc40
Ruby Salton <ruby@tornadofx.co.il>
2024-03-20 21:56:09 +0200

    "Builder" design pattern - trivial solution

--- design-patterns.tests/BuilderTests.cs
+++ design-patterns.tests/BuilderTests.cs
@@ -1,3 +1,5 @@
+using design_patterns.Builder;
+using design_patterns.Builder.contracts;
 using Xunit;
 
 namespace design_patterns.tests;
@@ -7,12 +9,22 @@ public class BuilderTests
     [Fact]
     public void TestBuildJeep()
     {
-        Assert.Fail("TODO");
+        ICarBuilder carBuilder = new CarBuilder();
+        ICar jeep = carBuilder.BuildCar("Jeep");
+        Assert.Equal($"body: Jeep\n" +
+                     $"engine horsepower: 400\n" +
+                     $"number of wheels: 4\n" +
+                     $"wheel size: 22\"", jeep.Specification);
     }
 
     [Fact]
     public void TestBuildHatchback()
     {
-        Assert.Fail("TODO");
+        ICarBuilder carBuilder = new CarBuilder();
+        ICar hatchback = carBuilder.BuildCar("Hatchback");
+        Assert.Equal($"body: Hatchback\n" +
+                     $"engine horsepower: 85\n" +
+                     $"number of wheels: 4\n" +
+                     $"wheel size: 16\"", hatchback.Specification);
     }
 }
