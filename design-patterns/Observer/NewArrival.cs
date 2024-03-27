a9771133133885a949c82bd01fcc5a75d86f774f
Ruby Salton <ruby@tornadofx.co.il>
2024-03-27 21:41:24 +0200

    The "Observer" design pattern

---
+++ design-patterns/Observer/NewArrival.cs
@@ -0,0 +1,9 @@
+﻿using design_patterns.Observer.contracts;
+
+namespace design_patterns.Observer;
+
+public class NewArrival(string title, InventoryType inventoryType) : INewArrival
+{
+    public string Title => title;
+    public InventoryType Type => inventoryType;
+}
