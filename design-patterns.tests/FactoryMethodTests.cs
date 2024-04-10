ac89693269805a055ccea92de327dc02f46d32bd
Ruby Salton <ruby@tornadofx.co.il>
2024-04-10 19:57:24 +0300

    The "Factory Method" design pattern

---
+++ design-patterns.tests/FactoryMethodTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace design_patterns.tests;
+
+public class FactoryMethodTests
+{
+    [Fact]
+    public void TestTrivialSolution()
+    {
+        Assert.Fail("TODO");
+    }
+}
