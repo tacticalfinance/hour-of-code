6f470f4680c141e681bd75949a0aab1155b250b7
Ruby Salton <ruby@tornadofx.co.il>
2024-05-22 21:12:05 +0300

    The "Abstract Factory" design pattern

---
+++ design-patterns.tests/AbstractFactoryTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace design_patterns.tests;
+
+public class AbstractFactoryTests
+{
+    [Fact]
+    public void TestLightTheme()
+    {
+        Assert.Fail("TODO");
+    }
+
+    [Fact]
+    public void TestDarkTheme()
+    {
+        Assert.Fail("TODO");
+    }
+}
