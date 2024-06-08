81943547e88cb6b0f5ff05ff6a2893335a2d8ce2
Ruby Salton <ruby@tornadofx.co.il>
2024-06-08 21:12:30 +0300

    The "Decorator" design pattern

---
+++ design-patterns.tests/DecoratorTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace design_patterns.tests;
+
+public class DecoratorTests
+{
+    [Fact]
+    public void TestWeaponWithRedDotSight()
+    {
+    }
+
+    [Fact]
+    public void TestWeaponWithBipods()
+    {
+    }
+
+    [Fact]
+    public void TestWeaponWithFlashlight()
+    {
+    }
+
+    [Fact]
+    public void TestWeaponWithBipodsAndFlashlight()
+    {
+    }
+}
